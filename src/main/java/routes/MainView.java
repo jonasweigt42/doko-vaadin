83fb1fc5d8892a19bf5c83018a10a9b7f3b083b4
Jonas W <evahank@Evas-MacBook-Pro.local>
2020-04-06 21:42:13 +0200

    added new route

---
+++ src/main/java/routes/MainView.java
@@ -0,0 +1,52 @@
+package routes;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import com.doko.jonas.DataService;
+import com.doko.jonas.dataTypes.Game;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * The main view contains a simple label element and a template element.
+ */
+@Route("")
+@PWA(name = "Project Base for Vaadin Flow with CDI", shortName = "Project Base")
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private DataService dataService;
+
+	@PostConstruct
+	public void init() throws IOException, URISyntaxException
+	{
+		Button button = new Button("Players");
+		button.addClickListener(e -> button.getUI().ifPresent(ui->ui.navigate("playerView")));
+		// Button click listeners can be defined as lambda expressions
+		// Button button = new Button("Say hello", e -> Notification.show(greetService.greet(textField.getValue())));
+		
+		Grid<Game> gameGrid = new Grid<>(Game.class);
+		gameGrid.setItems(dataService.getAllGames());
+
+		Notification.show("Daten für " + dataService.getGameCount() + " Spiele geladen...");
+
+		addClassName("centered-content");
+
+		add(button, gameGrid);
+	}
+
+}
--- src/main/java/routes/PlayerView.java
+++ src/main/java/routes/PlayerView.java
@@ -1,44 +1,33 @@
-package com.doko.jonas;
+package routes;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 import com.doko.jonas.dataCollect.PlayerPool;
-import com.doko.jonas.dataTypes.Game;
 import com.doko.jonas.dataTypes.Player;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
 
-/**
- * The main view contains a simple label element and a template element.
- */
-@Route("")
-@PWA(name = "Project Base for Vaadin Flow with CDI", shortName = "Project Base")
+@Route("playerView")
 @CssImport("./styles/shared-styles.css")
-@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout
+@CssImport(value = "./styles/vaadin-text-field-styles.css")
+public class PlayerView extends VerticalLayout
 {
 
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	private DataService dataService;
-
 	@PostConstruct
 	public void init() throws IOException, URISyntaxException
 	{
-		Grid<Game> gameGrid = new Grid<>(Game.class);
-		gameGrid.setItems(dataService.getAllGames());
-
-		Notification.show("Daten für " + dataService.getGameCount() + " Spiele geladen...");
+		
+		Button button = new Button("back");
+		button.addClickListener(e -> button.getUI().ifPresent(ui->ui.navigate("")));
 
 		List<Player> players = PlayerPool.getAllPlayers();
 		Grid<Player> playerGrid = new Grid<>(Player.class);
@@ -46,7 +35,8 @@ public class MainView extends VerticalLayout
 
 		addClassName("centered-content");
 
-		add(playerGrid, gameGrid);
-	}
+		add(button, playerGrid);
 
+	}
+	
 }
