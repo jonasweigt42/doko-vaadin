c4e105f5b5f8d639ec40717d787de5cd639537f1
Jonas W <evahank@Evas-MacBook-Pro.local>
2020-04-17 22:02:08 +0200

    added stats for players

--- src/main/java/com/doko/jonas/routes/PlayerView.java
+++ src/main/java/com/doko/jonas/routes/PlayerView.java
@@ -1,5 +1,7 @@
 package com.doko.jonas.routes;
 
+import java.text.DecimalFormat;
+
 import javax.inject.Inject;
 
 import com.doko.jonas.dataTypes.Player;
@@ -26,18 +28,27 @@ public class PlayerView extends VerticalLayout implements HasUrlParameter<String
 	@Override
 	public void setParameter(BeforeEvent event, String parameter)
 	{
+		DecimalFormat df = new DecimalFormat("0.00");
 		H2 label = new H2(parameter);
 		Player player = dataService.getPlayerByName(parameter);
 		
+		int anzahlSpiele = dataService.getGameCount();
 		Label score = new Label("overallScore: " + player.getOverallScore());
 		
+		Label label2 = new Label(dataService.getWonPercentagePerPlayer(player) + " % gewonnen");
+		Label label3 = new Label(dataService.getWonPercentagePerPlayerWhenStart(player) + " % gewonnen und rausgekommen");
+		Label label4 = new Label("Punkte pro Spiel: " + df.format((double) player.getOverallScore() / (double) anzahlSpiele));
+		Label label5 = new Label("Anzahl Solos: " + dataService.getSolosForPlayer(player).size() + " - " + dataService.getSoloWonPercentagePerPlayer(player) + "% gewonnen");
+
+		Label label6 = new Label("Spiele gegeben: " + dataService.getGamesDealtByPlayer(player).size());
+
 		Button backButton = new Button("back");
 		backButton.addClickListener(e -> backButton.getUI().ifPresent(ui -> ui.navigate("playersView")));
 		backButton.addClassName("bottom-content");
 
 		addClassName("centered-content");
 
-		add(label, score, backButton);
+		add(label, score, label2, label3, label4, label5, label6, backButton);
 	}
 
 }
